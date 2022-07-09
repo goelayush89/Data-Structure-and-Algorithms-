ce524e67d467593710bbead95c67215c7b8e1755
Ayush Goel <goela093@gmail.com>
2022-07-10 00:16:37 +0530

    Time: 72 ms (65.97%), Space: 57.3 MB (50.63%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,51 +1,64 @@
 class Solution {
 public:
     
-   
-bool compare(vector<int> &selected, string &currString) {
-    vector<int> selfCheck(26, 0);
-    for(int i = 0; i < currString.size(); i++) {
-        if(selfCheck[currString[i] - 'a'] == 1) return false;
-        selfCheck[currString[i] - 'a'] = 1;
-    }
-
-    for(int i = 0; i < currString.size(); i++) {
-        if(selected[currString[i] - 'a'] == 1) return false;
-    }
-    return true;
-}
-
-int help(int i, vector<string> &arr, vector<int> &selected, int len) {
-    
-    if(i == arr.size()) {
-        return len;
-    }
-    string currString = arr[i];
-    if(!compare(selected, currString)) {
-        return help(i + 1, arr, selected, len);
-    }
-    else {
-        for(int j = 0; j < currString.size(); j++) {
-            selected[currString[j] - 'a'] = 1;
-        }
-        len += currString.size();
-        int op1 = help(i + 1, arr, selected, len);
+    bool compare(string &check , vector<int>&selected){
+        vector<int>Selfselected(26,0);
+        int n = check.size();
+        for(int i=0;i<n;i++){
+            if(Selfselected[check[i]-'a']==1) return false;
         
-        for(int j = 0; j < currString.size(); j++) {
-            selected[currString[j] - 'a']  = 0;
+           Selfselected[check[i]-'a']=1;
+            
+            
         }
-
-        len -= currString.size();
-        int op2 = help(i + 1, arr, selected, len);
-
-        return max(op1, op2);
+        
+         for(int i=0;i<n;i++){
+            if(selected[check[i]-'a']==1) return false;
+       
+        }
+        return true;
+        
+        
+        
     }
-}
-
+    
+    int find(int i, vector<string>& arr, vector<int>&selected,int length){
+        
+        if(i==arr.size()) return length;
+        
+        
+       if(compare(arr[i],selected)==false){
+           
+         return  find(i+1,arr,selected,length);
+       }
+           
+           
+           length+=arr[i].length();
+           
+          for(int k=0;k<arr[i].length();k++){
+            selected[arr[i][k]-'a']=1;
+       
+        }
+         int op1 =   find(i+1,arr,selected,length);
+           
+           
+         
+           length-=arr[i].length();
+           
+          for(int k=0;k<arr[i].length();k++){
+            selected[arr[i][k]-'a']=0;
+       
+        }
+         int op2=   find(i+1,arr,selected,length);
+       
+        
+        return max(op1,op2);
+        
+    }
     int maxLength(vector<string>& arr) {
         vector<int>selected(26,0);
-        
-        int ans1 =help(0,arr,selected,0);
+        string ans;
+        int ans1 =find(0,arr,selected,0);
         
         return ans1;
         
