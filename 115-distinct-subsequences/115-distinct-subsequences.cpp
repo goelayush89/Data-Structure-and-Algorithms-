01f7e41246528692342139d8b2250026d3048174
Ayush Goel <goela093@gmail.com>
2022-10-07 18:59:00 +0530

    Time: 117 ms (9.39%), Space: 30 MB (31.62%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
-    
-    int f(int i , int j ,string &s, string &t, vector<vector<int>>&dp){
-        
-        if(j==0) return 1;
-        if(i==0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i-1]==t[j-1]){
-            return dp[i][j]=f(i-1 ,j-1,s,t,dp)+f(i-1,j,s,t,dp);
-        }
-        return dp[i][j] = f(i-1,j,s,t,dp);
-    }
-    int numDistinct(string s, string t) {
-        int n = s.length();
-        int m = t.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        
-        
-        
-        return f(n,m,s,t,dp);
+
+     int numDistinct(string s, string t) {
+        int m = t.length(), n = s.length();
+        vector<vector<double>> dp(m + 1, vector<double> (n + 1, 0));
+        for (int j = 0; j <= n; j++) dp[0][j] = 1;
+        for (int j = 1; j <= n; j++)
+            for (int i = 1; i <= m; i++)
+                dp[i][j] = dp[i][j - 1] + (t[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : 0);
+        return (int)dp[m][n];
     }
 };
