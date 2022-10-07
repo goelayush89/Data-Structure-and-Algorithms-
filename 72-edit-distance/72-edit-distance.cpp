6355c25094e82d7b12d9707fa8e78bbca41fd00f
Ayush Goel <goela093@gmail.com>
2022-10-07 19:22:32 +0530

    Time: 125 ms (16.35%), Space: 80.7 MB (11.61%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     
     int f(int i , int j , string word1, string word2, vector<vector<int>>&dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
+        if(i==0) return j;
+        if(j==0) return i;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(word1[i] == word2[j]){
+        if(word1[i-1] == word2[j-1]){
            return dp[i][j] = f(i-1,j-1,word1,word2,dp);
             
         }else{
@@ -19,7 +19,7 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.length();
         int m = word2.length();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,word1,word2,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return f(n,m,word1,word2,dp);
     }
 };
