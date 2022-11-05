d8fd62b1e160a1944d363fd5790f280bdd697cfd
Ayush Goel <goela093@gmail.com>
2022-11-05 15:43:55 +0530

    Time: 29 ms (83.14%), Space: 13.8 MB (65.20%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+          
+        
+        vector<int>adj[numCourses];
+                                                                        
+    for(auto it : prerequisites){
+        
+        adj[it[1]].push_back(it[0]);
+       
+    }
+       
+                                                                        vector<int>topo;
+         vector<int>indegree(numCourses,0);                                                              
+                                                                        
+                                                                        for(int i=0;i<numCourses; i++){
+                                                                            for(auto it : adj[i]){
+                                                                                indegree[it]++;
+                                                                            }
+                                                                        }
+            
+                                                                        
+                                                                       
+queue<int>q;
+                                                                        for(int i=0;i<numCourses;i++){
+
+  if(indegree[i]==0) q.push(i);                                                                       }
+                    
+                                                                        
+                                                                        while(!q.empty())
+    {
+        
+        int node = q.front();
+        q.pop();
+        topo.push_back(node);
+        
+        for(auto it : adj[node]){
+            indegree[it]--;
+            if(indegree[it]==0) q.push(it);
+        }
+    }
+           
+                                                                        cout << topo.size();
+                                                                        if(topo.size()==numCourses) return topo;
+ return { };
+                                                                        
+        
+    }
+};
