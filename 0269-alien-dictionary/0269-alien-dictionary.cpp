e5fcc33f4ab98346a1129fba6725529ef643bf4d
Ayush Goel <goela093@gmail.com>
2022-11-05 19:07:17 +0530

    Time: 11 ms (38.64%), Space: 9.5 MB (81.34%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.cpp
+++ 0269-alien-dictionary/0269-alien-dictionary.cpp
@@ -1,66 +1,78 @@
 class Solution {
 public:
+
     string alienOrder(vector<string>& words) {
-        int n = words.size();
-        unordered_map<char,int> indegree;
-        unordered_map<char,vector<char>> adj;
-        
-        for(auto word: words)
-        {
-            for(auto c: word)
-            {
-                indegree[c]=0;
-            }
-        }
-        
-        for(auto it : indegree) cout << it.first << it.second;
-        //create graph
-        string prevword;
-        string curword;
-        for(int k=1;k<n;k++)
-        {
-            prevword = words[k-1];
-            curword = words[k];
-            int i=0,j=0;
-            while(i<prevword.size() and j<curword.size())
-            {
-                if(prevword[i]!=curword[j])
-                {
-                    adj[prevword[i]].push_back(curword[j]);
-                    indegree[curword[j]]++;
-                    break;
-                }
-                i++; j++;
-            }
-            if(i!=prevword.size() and j==curword.size()) return "";
-        }
-        
-        queue<char> q;
-        for(auto i: indegree)
-        {
-            if(i.second==0)
-            {
-                q.push(i.first);
-            }
-        }
-        
-        //topo sort
-        
-        string res;
-        while(!q.empty())
-        {
-            char i = q.front();
-            q.pop();
-            res+=i;
-            for(int j=0;j<adj[i].size();j++)
-            {
-                indegree[adj[i][j]]--;
-                if(indegree[adj[i][j]]==0)
-                    q.push(adj[i][j]);
-            }
-        }
-        
+           unordered_map<char ,int>indegree;  
+        for(auto word : words){
+           for(auto ch : word){
+               indegree[ch]=0;
+           }
+       }
       
-        return (res.size()==indegree.size()) ? res : "";
+      unordered_map<char, vector<char>>graph;
+        
+          
+     for(int i=0;i<words.size()-1;i++){
+        string s1 = words[i];
+        string s2 = words[i+1];
+         int minSize = min(s1.length(),s2.length());
+         int cnt=0;
+        for(int j=0;j<minSize;j++){
+             indegree[s1[j]]=0;
+                indegree[s2[j]]=0;
+            if(s1[j]!=s2[j]){
+                graph[s1[j]].push_back(s2[j]);
+               
+                cnt++;
+                break;
+                    
+            }
+            
+        }
+         if(cnt==0 && s1.length() > s2.length()) return "";
+            
+     }
+
+    vector<char>topo;
+                                                
+                                                                        
+        for(auto it : graph){
+            for(auto x : it.second){
+                indegree[x]++;
+            }
+        }
+
+        for(auto it : indegree){
+           cout << it.first  << it.second << "\n";
+        }
+                                                         
+                                                                       
+queue<char>q;
+                                                        
+        for(auto it : indegree){
+            if(it.second==0) q.push(it.first);
+        }
+                    
+                                                                        while(!q.empty())
+    {
+        
+      char node = q.front();
+        q.pop();
+        topo.push_back(node);
+        
+        for(auto it : graph[node]){
+            indegree[it]--;
+            if(indegree[it]==0) q.push(it);
+        }
+    }
+           
+                                                                        cout << topo.size();
+ string ans ="";                                                        for(int i=0 ;i<topo.size();i++){
+                                                                         ans.push_back(topo[i]);
+                                                                        }
+   return ans.size() == indegree.size() ? ans : "";
+       
+        
+        
     }
 };
